e3acde7609710b7fa05a76bf5a75ad365ebbcb71
Jatin_Saini <sainijatin247@gmail.com>
2022-09-09 19:42:37 -0400

    heroku app deployed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {animals} = require('./data/animals.json');
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray){
@@ -52,7 +52,7 @@ app.get('/api/animals',(req,res) => {
 } )
 
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
     
 });
